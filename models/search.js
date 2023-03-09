0489ea0f8432fc3dc5f170ac73a43fe68451c056
Rae7771653 <nicole831220@gmail.com>
2023-03-09 13:58:41 +0800

    feat:add search model

--- models/search.js
+++ models/search.js
@@ -3,25 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Element extends Model {
+  class Search extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Element.hasMany(models.Note, { foreignKey: 'elementId' })
-      Element.hasMany(models.Favorite, { foreignKey: 'elementId' })
-      Element.hasMany(models.Search, { foreignKey: 'elementId' })
+      Search.belongsTo(models.Element, { foreignKey: 'elementId' })
     }
   }
-  Element.init({
-    name: DataTypes.STRING
+  Search.init({
+    elementId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Element',
-    tableName: 'Elements',
+    modelName: 'Search',
+    tableName: 'Searches',
     underscored: true
   });
-  return Element;
+  return Search;
 };
