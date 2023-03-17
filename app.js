923ef862d3039e6fbce269d3585f1e014fdd1663
Rae7771653 <nicole831220@gmail.com>
2023-03-17 12:55:04 +0800

    feat: add cors config in app.js

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const port = process.env.PORT || 3000
 const passport = require('./config/passport')
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowHeaders: ['Content-Type', 'Authorization']
+}
 const { updateLaw } = require('./helpers/lawUpdate-helpers')
 const routes = require('./routes')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use(passport.initialize())
+
 // 更新法規異動
-// let updateDate = '112-01-06'// json檔建立時間
-// updateLaw(updateDate)
+let updateDate = '112-01-06'// json檔建立時間
+updateLaw(updateDate)
 
 app.use(routes)
 app.listen(port, () => console.log(`It is listening on http://localhost:${port}`))
