f48169188d7603d1a84e9b5d86cebf3d8939b075
Rae7771653 <nicole831220@gmail.com>
2023-04-06 22:45:52 +0800

    feat:add .travis.yml

--- config/config.js
+++ config/config.js
@@ -14,7 +14,6 @@ module.exports = {
   },
   test: {
     username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql',
