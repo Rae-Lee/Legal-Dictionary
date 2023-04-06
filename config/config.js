a994e71f0c3eac63b6e5da5c327bccdcafad207a
Rae7771653 <nicole831220@gmail.com>
2023-04-06 22:55:49 +0800

    feat: add password in mysql

--- config/config.js
+++ config/config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   test: {
     username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql',
